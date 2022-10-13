6370fe1cec6f66d3f0cd48792fdf6a1ffabe168c
chigosix <tngusrldnjs1@gmail.com>
2022-10-13 20:33:17 +0900

    fix: build error
    1. tsconfig.json 수정
    - add:  "jsxImportSource": "@emotion/react",

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,41 +1,44 @@
+import { css } from "@emotion/react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "3rem",
-        width: "100%",
-        color: "#c9d1d9",
-        backgroundColor: "#0d1117",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-          cursor: "pointer",
-        }}
-      >
-        <Link href={`https://github.com/chigomuh/666666`}>
-          <a>
-            <Image
-              src={`/svg/github-icon.svg`}
-              alt={`github-icon`}
-              width={32}
-              height={32}
-            />
-          </a>
-        </Link>
-      </div>
-    </footer>
+    <>
+      <footer css={footer}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+            cursor: "pointer",
+          }}
+        >
+          <Link href={`https://github.com/chigomuh/666666`}>
+            <a>
+              <Image
+                src={`/svg/github-icon.svg`}
+                alt={`github-icon`}
+                width={32}
+                height={32}
+              />
+            </a>
+          </Link>
+        </div>
+      </footer>
+    </>
   );
 };
 
 export default Footer;
+
+const footer = css({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "3rem",
+  width: "100%",
+  color: "#c9d1d9",
+  backgroundColor: "#0d1117",
+});
